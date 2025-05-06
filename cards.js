359a8c338cc42d8f35594fd982eb6974fffb0fc4
soareswagner533@gmail.com <soareswagner533@gmail.com>
2025-05-06 16:58:09 -0300

    adicão das demais raridades e atualização da paginal principal html

--- cards.js
+++ cards.js
@@ -15,6 +15,78 @@ const lendario = [
     "assets/cards/Épico/Tracy McGrady epica.png",
     "assets/cards/Épico/Westbrook-epica.png"
   ];
+
+  const raro = [
+    "assets/cards/Raro/Amar-rara.png",
+    "assets/cards/Raro/Billups-rara.png",
+    "assets/cards/Raro/Davis-rara.png",
+    "assets/cards/Raro/DeronWilliams-rara.png",
+    "assets/cards/Raro/DraymondGreen-rara.png",
+    "assets/cards/Raro/DwightHoward-rara.png",
+    "assets/cards/Raro/JimmyButler-rara.png",
+    "assets/cards/Raro/JohnWall-rara.png",
+    "assets/cards/Raro/KevinLove-rara.png",
+    "assets/cards/Raro/KlayThompson-rara.png",
+    "assets/cards/Raro/Manu-Ginóbili-rara.png",
+    "assets/cards/Raro/Pierce-rara.png",
+    "assets/cards/Raro/ShawnKemp-rara.png",
+    "assets/cards/Raro/TonyParker-rara.png",
+    "assets/cards/Raro/VinceCarter-rara.png",
+  ];
+
+  const comum = [
+    "assets/cards/Comum/Al Horford comum.png",
+    "assets/cards/Comum/Andre Iguodala comum.png",
+    "assets/cards/Comum/Blake Griffin comum.png",
+    "assets/cards/Comum/Brook Lopez comum.png",
+    "assets/cards/Comum/Chris Bosh comum.png",
+    "assets/cards/Comum/Danny Green comum.png",
+    "assets/cards/Comum/DeMar DeRozan comum.png",
+    "assets/cards/Comum/Derek Fisher comum.png",
+    "assets/cards/Comum/Eric Gordon comum.png",
+    "assets/cards/Comum/Harrison Barnes comum.png",
+    "assets/cards/Comum/J.J. Redick comum.png",
+    "assets/cards/Comum/Jamal Crawford comum.png",
+    "assets/cards/Comum/Joe Johnson comum.png",
+    "assets/cards/Comum/LaMarcus Aldridge comum.png",
+    "assets/cards/Comum/Michael Finley comum.png",
+    "assets/cards/Comum/Monta Ellis comum.png",
+    "assets/cards/Comum/Pascal Siakam comum.png",
+    "assets/cards/Comum/Peja Stojaković comum.png",
+    "assets/cards/Comum/Rajon Rondo comum.png",
+    "assets/cards/Comum/Ricky Rubio comum.png",
+    "assets/cards/Comum/Rudy Gay comum.png",
+    "assets/cards/Comum/Serge Ibaka comum.png",
+    "assets/cards/Comum/Tobias Harris comum.png",
+  ];
+
+  const comum2 = [
+    "assets/cards/Comum/Al Horford comum.png",
+    "assets/cards/Comum/Andre Iguodala comum.png",
+    "assets/cards/Comum/Blake Griffin comum.png",
+    "assets/cards/Comum/Brook Lopez comum.png",
+    "assets/cards/Comum/Chris Bosh comum.png",
+    "assets/cards/Comum/Danny Green comum.png",
+    "assets/cards/Comum/DeMar DeRozan comum.png",
+    "assets/cards/Comum/Derek Fisher comum.png",
+    "assets/cards/Comum/Eric Gordon comum.png",
+    "assets/cards/Comum/Harrison Barnes comum.png",
+    "assets/cards/Comum/J.J. Redick comum.png",
+    "assets/cards/Comum/Jamal Crawford comum.png",
+    "assets/cards/Comum/Joe Johnson comum.png",
+    "assets/cards/Comum/LaMarcus Aldridge comum.png",
+    "assets/cards/Comum/Michael Finley comum.png",
+    "assets/cards/Comum/Monta Ellis comum.png",
+    "assets/cards/Comum/Pascal Siakam comum.png",
+    "assets/cards/Comum/Peja Stojaković comum.png",
+    "assets/cards/Comum/Rajon Rondo comum.png",
+    "assets/cards/Comum/Ricky Rubio comum.png",
+    "assets/cards/Comum/Rudy Gay comum.png",
+    "assets/cards/Comum/Serge Ibaka comum.png",
+    "assets/cards/Comum/Tobias Harris comum.png",
+  ];
+
+
   
   function sorteioCards(array) {
     const indice = Math.floor(Math.random() * array.length);
@@ -22,10 +94,16 @@ const lendario = [
   }
   
   function sorteioPacotes() {
-    const imgLendariosrc = sorteioCards(lendario);
-    const imgEpicosrc = sorteioCards(epico);
+    const imgLendario = sorteioCards(lendario);
+    const imgEpico = sorteioCards(epico);
+    const imgraro = sorteioCards(raro);
+    const imgcomum = sorteioCards(comum);
+    const imgcomum2 = sorteioCards(comum2);
   
-    document.getElementById("imagem1").src = imgLendariosrc;
-    document.getElementById("imagem2").src = imgEpicosrc;
+    document.getElementById("carta1").src = imgLendario;
+    document.getElementById("carta2").src = imgEpico;
+    document.getElementById("carta3").src = imgraro;
+    document.getElementById("carta4").src = imgcomum;
+    document.getElementById("carta5").src = imgcomum2
   }
   
